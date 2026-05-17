3b5a08fdce1c402b13f508db64f024a9b931f3c6
Kelgors <github.hurt819@passmail.net>
2026-05-17 13:43:31 +0000

    Add prisma

--- packages/system/src/env.ts
+++ packages/system/src/env.ts
@@ -6,6 +6,7 @@ export const appStageSchema = z.enum(["dev", "prod", "test"]);
 export const env = z
   .object({
     APP_STAGE: appStageSchema,
+    DATABASE_URL: z.url(),
   })
   .parse(process.env);
 
--- packages/system/src/index.ts
+++ packages/system/src/index.ts
@@ -1,15 +1,32 @@
+import { prisma } from "./prisma.js";
 import { createServer } from "./server.js";
 
-export function start() {
+export async function start() {
+  await prisma.$connect();
   const server = createServer();
+
+  async function exit() {
+    await server.close();
+    await prisma.$disconnect();
+  }
+
+  async function onExitSignal(signal: NodeJS.Signals | "exit") {
+    server.log.info(`Received ${signal}, shutting down gracefully...`);
+    process.off("SIGINT", onExitSignal);
+    process.off("SIGTERM", onExitSignal);
+    await exit();
+  }
+  process.once("SIGINT", onExitSignal);
+  process.once("SIGTERM", onExitSignal);
 
   server.listen({ port: 3000 }, (err, address) => {
     if (err) {
       server.log.error(err);
-      process.exit(1);
+      void exit().finally(() => process.exit(1));
+      return;
     }
     server.log.info(`Server listening at ${address}`);
   });
 }
 
-start();
+void start();
