72d1553ae84a89395a307b73b0849be1cd963477
Alex <alex.dana2013@gmail.com>
2026-06-12 23:56:03 +0200

    Prettier the code

--- packages/game-client/src/ecs/systems/input.ts
+++ packages/game-client/src/ecs/systems/input.ts
@@ -7,13 +7,16 @@ import { moveOnClickSystem } from "./moveOnClick.js";
 export function inputSystem(world: GameWorld): void {
   for (const click of drainClicks()) {
     moveOnClickSystem(world, click);
-
-    const command = PlayerCommand.encode({
-      type: GameCommandType.GAME_COMMAND_TYPE_MOVE_TO,
-      action: "MOVE_TO",
-      payload: new TextEncoder().encode(JSON.stringify({ x: click.x, y: click.y })),
-    }).finish();
-
-    sendMessage(command);
+    sendMoveToCommand(click.x, click.y);
   }
+}
+
+function sendMoveToCommand(x: number, y: number) {
+  const command = PlayerCommand.encode({
+    type: GameCommandType.GAME_COMMAND_TYPE_MOVE_TO,
+    action: "MOVE_TO",
+    payload: new TextEncoder().encode(JSON.stringify({ x, y })),
+  }).finish();
+
+  sendMessage(command);
 }
