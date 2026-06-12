27449862f31e31cb7c0bafd5536beb332b456964
Alex <alex.dana2013@gmail.com>
2026-06-12 19:52:12 +0200

    Create commit :D

--- packages/game-client/src/ecs/observers/index.ts
+++ packages/game-client/src/ecs/observers/index.ts
@@ -1,6 +1,6 @@
+import { registerTransformObservers } from "@universe/ecs-shared";
 import type { World } from "bitecs";
 import { registerSpriteObservers } from "./sprite.js";
-import { registerTransformObservers } from "./transform.js";
 
 export function registerObservers(world: World): void {
   registerTransformObservers(world);
--- packages/game-client/src/ecs/systems/render.ts
+++ packages/game-client/src/ecs/systems/render.ts
@@ -1,8 +1,8 @@
+import { Transform } from "@universe/ecs-shared";
 import { query } from "bitecs";
 import type { Container } from "pixi.js";
 import type { GameWorld } from "../../plugins/bitecs.js";
 import { Sprite } from "../components/sprite.js";
-import { Transform } from "../components/transform.js";
 
 export function renderSystem(world: GameWorld, container: Container) {
   for (const eid of query(world, [Transform, Sprite])) {
